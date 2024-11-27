351a8fe593c264dbb26f07eef421c096127abf55
quoclvv <vuquoc.dev@gmail.com>
2024-11-27 11:03:07 +0700

    fix: correct endpoint

--- dist/constants/endpoints.d.ts
+++ dist/constants/endpoints.d.ts
@@ -1,6 +1,6 @@
 export declare const API_ENDPOINTS: {
     readonly GENERATE_QR: "/api/v1/external_transactions/generate_qr";
     readonly CHECK_TRANSACTION: "/api/v1/external_transactions/check_transaction";
-    readonly GET_USER: "/api/v1/users";
-    readonly UPDATE_CREDITS: "/api/v1/users/update_credits";
+    readonly GET_USER: "/api/v1/user";
+    readonly UPDATE_CREDITS: "/api/v1/user/update_credits";
 };
--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,6 +1,6 @@
 export const API_ENDPOINTS = {
   GENERATE_QR: '/api/v1/external_transactions/generate_qr',
   CHECK_TRANSACTION: '/api/v1/external_transactions/check_transaction',
-  GET_USER: '/api/v1/users',
-  UPDATE_CREDITS: '/api/v1/users/update_credits'
+  GET_USER: '/api/v1/user',
+  UPDATE_CREDITS: '/api/v1/user/update_credits'
 } as const;
